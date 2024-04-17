6b9b0ef65ac91e43e77889bc6f8f4597749dd5bb
tangxiaoxin <1302947749@qq.com>
2024-04-17 21:26:31 +0800

    notify

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Command } from "commander";
 import downloadRepo from "./download-repo";
 import packageJson from "../package.json";
+import updateNotifier from "update-notifier-cjs";
 import log from "./log";
+import chalk from "chalk";
 const program = new Command();
 
 program
@@ -9,8 +11,15 @@ program
   .version(packageJson.version)
   .argument("<dest>", "app name directory")
   .action(async (dest) => {
+    const notifier = updateNotifier({ pkg: packageJson }).notify();
     await downloadRepo(dest);
     log(dest);
+    if (notifier.update) {
+      console.log(
+        "\n",
+        chalk.yellowBright(`${notifier.update.latest} 版本已发布，请升级`)
+      );
+    }
   });
 
 program.parse(process.argv);
